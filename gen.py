6026ff89e29a2f2e93d0161ce10a4975f4901c28
JaredMathis <jared.mathis@native.consulting>
2023-01-28 12:41:52 -0500

    progress

--- gen.py
+++ gen.py
@@ -1,7 +1,8 @@
 from common import file_json_read, http_get_cached
+from urllib.parse import quote    
 
 words = file_json_read('../BibleVersions/gitignore/spanish_words.json')
 
 for word in words[:10]:
     print(word)
-    http_get_cached('https://en.wiktionary.org/wiki/' + word)
+    http_get_cached('https://en.wiktionary.org/wiki/' + quote(word))
